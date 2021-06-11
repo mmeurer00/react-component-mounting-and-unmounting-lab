78f2c19ed5caec5fea0c97d5f9a5f76729a6ced8
Maxine <maxinemeurer@yahoo.com>
2021-06-11 10:16:44 -0400

    commit tree 78a7092edcfbcdf96ad2ef7982f15f3479a1df29

--- src/components/Pancake.js
+++ src/components/Pancake.js
@@ -9,10 +9,15 @@ class Pancake extends React.Component {
       flippedAt: null
     };
   }
-
+//hey!!! testing
   // TODO: create a componentDidMount() which will start the interval to count how long the pancake has been cooking
-
+componentDidMount(){
+  this.startInterval()
+}
   // TODO: create a componentWillUnmount() which will clear the interval
+componentWillUnmount(){
+  this.cleanUpInterval()
+}
 
   updateCounter = () => {
     this.setState({
